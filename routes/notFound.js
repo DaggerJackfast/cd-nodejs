d083a141c705c8f9ce4466fb802288121908b811
Aibek <aibek.abdykasymov@gmail.com>
2018-02-01 01:00:20 +0600

    add templates

--- routes/notFound.js
+++ routes/notFound.js
@@ -1,12 +1,16 @@
-const fs = require('fs');
-const path = require('path');
+const render = require('../lib/render');
 
 
-
-function notFound(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    const stream = fs.createReadStream(path.resolve('public', 'error.html'));
-    stream.pipe(res);
+function notFound(req, res) {
+    render('error.html', {error: 'Not Found'}, (error, html) => {
+        if (error) {
+            res.writeHead(500, {'Content-Type': 'text/plain'})
+            return res.end(error.message)
+        }
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(html);
+    });
 }
-module.exports=notFound;
+
+module.exports = notFound;
--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,6 @@
-const fs = require('fs');
-const path = require('path');
 const url = require('url');
 const omdb = require('../lib/omdb');
+const render = require('../lib/render');
 
 
 function search(req, res) {
@@ -11,11 +10,29 @@ function search(req, res) {
     res.setHeader('Content-Type', 'text/html');
 
     omdb.get(title, (error, movie) => {
-        if (error) throw error;
-        console.log(movie);
+        if (error) {
+            return render('error.html', {error: error.message}, (error, html) => {
+                if (error) {
+                    res.writeHead(500, {'Content-Type': 'text/plain'})
+                    return res.end(error.message)
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                res.end(html);
+            });
+        }
+
+        render('movie.html', movie, (error, html) => {
+            if (error) {
+                res.writeHead(500, {'Content-Type': 'text/plain'})
+                return res.end(error.message)
+            }
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(html);
+        });
     });
-    const stream = fs.createReadStream(path.resolve('public', 'movie.html'));
-    stream.pipe(res);
+
 }
 
 module.exports = search;
